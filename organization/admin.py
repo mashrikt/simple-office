cf95cf1bf3b787d9c295819adc1864edb5fd903c
tm <mashrikt@gmail.com>
2017-08-18 05:18:06 +0600

    list display for Organization Link

--- organization/admin.py
+++ organization/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from organization.models import Organization, OrganizationLink
 
 
+class OrganizationLinkAdmin(admin.ModelAdmin):
+    list_display = ('organization', 'title', 'link')
+
 admin.site.register(Organization)
-admin.site.register(OrganizationLink)
+admin.site.register(OrganizationLink, OrganizationLinkAdmin)
